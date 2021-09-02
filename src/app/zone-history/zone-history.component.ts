34d5d9600fc03ebb9ca73e9af507122e7e3832e1
Kerry Shetline <kerry@shetline.com>
2021-09-01 20:21:11 -0400

    More complete transition history list.

--- src/app/zone-history/zone-history.component.ts
+++ src/app/zone-history/zone-history.component.ts
@@ -2,35 +2,53 @@ import { Component, OnInit } from '@angular/core';
 import { DateTime, Timezone, Transition, ttime } from '@tubular/time';
 import { max } from '@tubular/math';
 
+const NMSI = Number.MIN_SAFE_INTEGER;
+
 @Component({
   selector: 'tze-zone-history',
   templateUrl: './zone-history.component.html',
   styleUrls: ['./zone-history.component.scss']
 })
 export class ZoneHistoryComponent implements OnInit {
+  NMSI = NMSI;
+
   private _selectedTimezone: string;
 
   time = new DateTime();
   timezone: Timezone;
   transitions: Transition[] | null = [];
+  upcomingTransition = NMSI;
 
   constructor() {
     this.selectedTimezone = Timezone.guess();
   }
 
   ngOnInit(): void {
-    console.log('');
+    this.scrollToUpcoming();
   }
 
   get selectedTimezone(): string { return this._selectedTimezone; }
   set selectedTimezone(value: string) {
     if (this._selectedTimezone !== value) {
-      const twentyYearsFromNow = Date.now() + 631_152_000_000;
+      const now = Date.now();
+      const twentyYearsFromNow = now + 631_152_000_000;
 
       this._selectedTimezone = value;
       this.timezone = Timezone.getTimezone(value);
       this.time.timezone = this.timezone;
       this.transitions = this.timezone.getAllTransitions()?.reverse().filter(t => t.transitionTime < twentyYearsFromNow);
+      this.transitions.forEach(t => t.transitionTime > now && (this.upcomingTransition = t.transitionTime));
+      setTimeout(() => this.scrollToUpcoming());
+    }
+  }
+
+  scrollToUpcoming(): void {
+    const transList = document.querySelector('.transition-list') as HTMLElement;
+    const upcoming = transList?.querySelector('.upcoming-row') as HTMLElement;
+
+    if (upcoming) {
+      upcoming.scrollIntoView(true);
+      setTimeout(() => transList.scrollBy(0, -44));
     }
   }
 
@@ -39,12 +57,12 @@ export class ZoneHistoryComponent implements OnInit {
   }
 
   momentAfter(t: Transition, delta = 0): string {
-    if (t.transitionTime === Number.MIN_SAFE_INTEGER)
+    if (t.transitionTime === NMSI)
       return '—';
 
     this.time.utcTimeMillis = t.transitionTime - delta;
 
-    return this.time.format(ttime.DATETIME_LOCAL_SECONDS);
+    return this.time.format('ddd ' + ttime.DATETIME_LOCAL_SECONDS);
   }
 
   getTransitionIcon(t: Transition): string {
@@ -61,12 +79,12 @@ export class ZoneHistoryComponent implements OnInit {
   }
 
   offsetAfter(t: Transition, delta = 0): string {
-    if (t.transitionTime === Number.MIN_SAFE_INTEGER && delta !== 0)
+    if (t.transitionTime === NMSI && delta !== 0)
       return '—';
 
     this.time.utcTimeMillis = max(t.transitionTime - delta, -5_364_662_400_000);
 
-    return this.time.format('Z');
+    return this.time.format('Z').replace(/-/g, '\u2212');
   }
 
   dstBefore(t: Transition, asBoolean = false): string | boolean {
@@ -74,7 +92,7 @@ export class ZoneHistoryComponent implements OnInit {
   }
 
   dstAfter(t: Transition, asBoolean = false, delta = 0): string | boolean {
-    if (t.transitionTime === Number.MIN_SAFE_INTEGER && delta !== 0)
+    if (t.transitionTime === NMSI && delta !== 0)
       return '—';
 
     this.time.utcTimeMillis = t.transitionTime - delta;
@@ -82,6 +100,32 @@ export class ZoneHistoryComponent implements OnInit {
     if (this.time.dstOffsetSeconds === 0)
       return asBoolean ? false : 'no DST';
 
-    return asBoolean ? true : Timezone.formatUtcOffset(this.time.dstOffsetSeconds);
+    return asBoolean ? true : Timezone.formatUtcOffset(this.time.dstOffsetSeconds).replace(/-/g, '\u2212');
+  }
+
+  dstAfterStyle(t: Transition): string {
+    if (t.dstOffset < 0)
+      return 'negative-dst';
+    else if (t.dstOffset !== 0 && t.dstOffset !== 3600)
+      return 'irregular-dst';
+    else
+      return undefined;
+  }
+
+  abbreviationAfter(t: Transition): string {
+    this.time.utcTimeMillis = max(t.transitionTime, -5_364_662_400_000);
+
+    return this.time.format('z');
+  }
+
+  getRowMessage(t: Transition): string {
+    if (t.deltaOffset > 7200)
+      return 'Large transition: ' + Timezone.formatUtcOffset(t.deltaOffset);
+    else if (t.deltaOffset % 900 !== 0)
+      return 'Irregular transition: ' + Timezone.formatUtcOffset(t.deltaOffset);
+    else if (t.deltaOffset === 0 && t.transitionTime !== NMSI)
+      return 'Transition with no UTC offset change ';
+    else
+      return '';
   }
 }
